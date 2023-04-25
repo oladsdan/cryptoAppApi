5a4b80519a6274cb5d0b6e5af7e54f56b2948663
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-04-25 22:38:51 +0100

    adding a search input to the cryptocurrency component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input} from 'antd'
@@ -11,12 +11,25 @@ const Cryptocurrencies = ({simplified}) => {
   //destruction of the getcyptoquery
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+
+    const filteredData = cryptosList?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  },[cryptosList, searchTerm]) // this will execute when both changes
 
   if(isFetching) return 'Loading...'
  
 
   return (
     <>
+      {/* creating a search item */}
+      <div className="search-crypto">
+        <input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency)=> (
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
